411305876ea68214f025462d457d875a8abfbc44
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 22:25:11 -0300

    [ :white_check_mark: ] | create: test - verifier > tuple [point] (app::point)

--- the_ray_tracer_challenge/src/tuple/test/point.rs
+++ the_ray_tracer_challenge/src/tuple/test/point.rs
@@ -12,4 +12,17 @@ mod tests {
 
         assert_eq!(p.w, 1.0);
     }
+
+    #[test]
+    fn is_pointer_verifier() {
+        let p = Tuple::Point(1.0, 2.0, 3.0);
+
+        assert_eq!(p.is_point(), true);
+    }
+
+    fn not_is_pointer_verifier() {
+        let p = Tuple::Vector(1.0, 2.0, 3.0);
+
+        assert_eq!(p.is_point(), false);
+    }
 }
