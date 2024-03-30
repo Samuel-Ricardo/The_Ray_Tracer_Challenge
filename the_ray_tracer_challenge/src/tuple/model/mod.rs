2856e5375db7e6197f52ffb41ecfd65057ab405e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 22:19:42 -0300

    [ :sparkles: ] | create: module - tuple > verification (app::module)

--- the_ray_tracer_challenge/src/tuple/model/mod.rs
+++ the_ray_tracer_challenge/src/tuple/model/mod.rs
@@ -17,4 +17,12 @@ impl Tuple {
     pub fn Vector(x: f64, y: f64, z: f64) -> Tuple {
         Tuple { x, y, z, w: 0.0 }
     }
+
+    pub fn is_point(&self) -> bool {
+        self.w == 1.0
+    }
+
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
 }
