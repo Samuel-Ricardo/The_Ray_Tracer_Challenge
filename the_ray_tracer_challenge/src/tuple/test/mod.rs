c2c4245756cda63962f90a3d7386c88385a9b303
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 22:16:46 -0300

    [ :white_check_mark: ] | create: test - model > tuple (app::module)

--- the_ray_tracer_challenge/src/tuple/test/mod.rs
+++ the_ray_tracer_challenge/src/tuple/test/mod.rs
@@ -3,6 +3,16 @@ mod tests {
     use crate::tuple::model::Tuple;
 
     #[test]
+    fn tuple_initialize_correctly() {
+        let t = Tuple::new(1.0, 2.0, 3.0, 4.0);
+
+        assert_eq!(t.x, 1.0);
+        assert_eq!(t.y, 2.0);
+        assert_eq!(t.z, 3.0);
+        assert_eq!(t.w, 4.0);
+    }
+
+    #[test]
     fn point_initialize_correctly() {
         let p = Tuple::Point(1.0, 2.0, 3.0);
 
