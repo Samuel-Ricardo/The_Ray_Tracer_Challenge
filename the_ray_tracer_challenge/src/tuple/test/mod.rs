4e3baebcc7e9a2df995e10a63e073207a48a88ba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 22:21:35 -0300

    [ :recycle: ] | refact: tests - tuple

--- the_ray_tracer_challenge/src/tuple/test/mod.rs
+++ the_ray_tracer_challenge/src/tuple/test/mod.rs
@@ -14,26 +14,4 @@ mod tests {
         assert_eq!(t.z, 3.0);
         assert_eq!(t.w, 4.0);
     }
-
-    #[test]
-    fn point_initialize_correctly() {
-        let p = Tuple::Point(1.0, 2.0, 3.0);
-
-        assert_eq!(p.x, 1.0);
-        assert_eq!(p.y, 2.0);
-        assert_eq!(p.z, 3.0);
-
-        assert_eq!(p.w, 1.0);
-    }
-
-    #[test]
-    fn vector_initialize_correctly() {
-        let v = Tuple::Vector(1.0, 2.0, 3.0);
-
-        assert_eq!(v.x, 1.0);
-        assert_eq!(v.y, 2.0);
-        assert_eq!(v.z, 3.0);
-
-        assert_eq!(v.w, 0.0);
-    }
 }
