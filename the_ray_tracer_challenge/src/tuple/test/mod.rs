09fedcd0c4734a605add1dd0d00ae917ac130318
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 22:16:26 -0300

    [ :white_check_mark: ] | create: test - model > tuple [vector] (app::module)

--- the_ray_tracer_challenge/src/tuple/test/mod.rs
+++ the_ray_tracer_challenge/src/tuple/test/mod.rs
@@ -12,4 +12,15 @@ mod tests {
 
         assert_eq!(p.w, 1.0);
     }
+
+    #[test]
+    fn vector_initialize_correctly() {
+        let v = Tuple::Vector(1.0, 2.0, 3.0);
+
+        assert_eq!(v.x, 1.0);
+        assert_eq!(v.y, 2.0);
+        assert_eq!(v.z, 3.0);
+
+        assert_eq!(v.w, 0.0);
+    }
 }
