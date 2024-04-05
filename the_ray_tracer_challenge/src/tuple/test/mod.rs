40f8a4165553fc563cb569f1e31bcf26a9e344f6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-04 22:41:29 -0300

    [ :white_check_mark: ] | create: test - tuple > negative vectors (app::module)

--- the_ray_tracer_challenge/src/tuple/test/mod.rs
+++ the_ray_tracer_challenge/src/tuple/test/mod.rs
@@ -48,13 +48,10 @@ mod tests {
     }
 
     #[test]
-    fn sub_of_two_vectors() {
-        let t1 = Tuple::Vector(3.0, -2.0, 5.0);
-        let t2 = Tuple::Vector(-2.0, 3.0, 1.0);
+    fn negating_a_tuple() {
+        let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        let expected = Tuple::new(-1.0, 2.0, -3.0, 4.0);
 
-        let expected = Tuple::Vector(5.0, -5.0, 4.0);
-        let result = t1 - t2;
-
-        assert_eq!(result, expected);
+        assert_eq!(-t, expected);
     }
 }
