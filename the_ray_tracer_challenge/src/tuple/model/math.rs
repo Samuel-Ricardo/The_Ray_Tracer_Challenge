2a7f1dc89b88157740b1590360998f77e4030129
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-05 09:37:51 -0300

    [ :recycle: ] | refact: tuple - math (app::module)

---
+++ the_ray_tracer_challenge/src/tuple/model/math.rs
@@ -0,0 +1,54 @@
+use super::Tuple;
+use std::ops;
+
+impl ops::Add<Tuple> for Tuple {
+    type Output = Tuple;
+
+    fn add(self, other: Tuple) -> Tuple {
+        Tuple {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl ops::Sub<Tuple> for Tuple {
+    type Output = Tuple;
+
+    fn sub(self, other: Tuple) -> Self::Output {
+        Tuple {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+impl ops::Neg for Tuple {
+    type Output = Tuple;
+
+    fn neg(self) -> Self::Output {
+        Tuple {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            w: -self.w,
+        }
+    }
+}
+
+impl ops::Mul<f64> for Tuple {
+    type Output = Tuple;
+
+    fn mul(self, other: f64) -> Self::Output {
+        Tuple {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+            w: self.w * other,
+        }
+    }
+}
--- the_ray_tracer_challenge/src/tuple/model/mod.rs
+++ the_ray_tracer_challenge/src/tuple/model/mod.rs
@@ -1,4 +1,4 @@
-use std::ops;
+mod math;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Tuple {
@@ -27,57 +27,5 @@ impl Tuple {
 
     pub fn is_vector(&self) -> bool {
         self.w == 0.0
-    }
-}
-
-impl ops::Add<Tuple> for Tuple {
-    type Output = Tuple;
-
-    fn add(self, other: Tuple) -> Tuple {
-        Tuple {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-            w: self.w + other.w,
-        }
-    }
-}
-
-impl ops::Sub<Tuple> for Tuple {
-    type Output = Tuple;
-
-    fn sub(self, other: Tuple) -> Self::Output {
-        Tuple {
-            x: self.x - other.x,
-            y: self.y - other.y,
-            z: self.z - other.z,
-            w: self.w - other.w,
-        }
-    }
-}
-
-impl ops::Neg for Tuple {
-    type Output = Tuple;
-
-    fn neg(self) -> Self::Output {
-        Tuple {
-            x: -self.x,
-            y: -self.y,
-            z: -self.z,
-            w: -self.w,
-        }
-    }
-}
-
-impl ops::Mul<f64> for Tuple {
-    type Output = Tuple;
-
-    fn mul(self, other: f64) -> Self::Output {
-        Tuple {
-            x: self.x * other,
-            y: self.y * other,
-            z: self.z * other,
-            w: self.w * other,
-        }
     }
 }
