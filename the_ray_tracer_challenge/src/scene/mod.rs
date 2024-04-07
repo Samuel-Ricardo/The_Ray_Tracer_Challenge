a779377fea83e54f7675322e1a78a64dad16f521
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-07 00:39:12 -0300

    [ :sparkles: ] | create: tick (app::module::scene)

--- the_ray_tracer_challenge/src/scene/mod.rs
+++ the_ray_tracer_challenge/src/scene/mod.rs
@@ -1,1 +1,10 @@
-pub mod model;
+use self::model::{Environment, Projectile};
+
+pub mod model;
+
+fn tick(environment: &Environment, projectile: &Projectile) -> Projectile {
+    Projectile::new(
+        projectile.position + projectile.velocity,
+        projectile.velocity + environment.gravity + environment.wind,
+    )
+}
