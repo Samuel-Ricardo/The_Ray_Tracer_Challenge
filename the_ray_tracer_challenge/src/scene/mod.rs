b23e48d23a845b472bf6201518d503597000c2b7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-07 00:49:09 -0300

    [ :sparkles: ] | simulate: physics (app)

--- the_ray_tracer_challenge/src/scene/mod.rs
+++ the_ray_tracer_challenge/src/scene/mod.rs
@@ -2,7 +2,7 @@ use self::model::{Environment, Projectile};
 
 pub mod model;
 
-fn tick(environment: &Environment, projectile: &Projectile) -> Projectile {
+pub fn tick(environment: &Environment, projectile: &Projectile) -> Projectile {
     Projectile::new(
         projectile.position + projectile.velocity,
         projectile.velocity + environment.gravity + environment.wind,
