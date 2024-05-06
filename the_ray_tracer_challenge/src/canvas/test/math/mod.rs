281dc03bf7230f74a261d539d50bce02a718ae5a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-06 16:30:07 -0300

    [ :white_check_mark: ] | create: test - color > sub (app::canvas)

--- the_ray_tracer_challenge/src/canvas/test/math/mod.rs
+++ the_ray_tracer_challenge/src/canvas/test/math/mod.rs
@@ -9,4 +9,12 @@ mod test {
 
         assert_eq!(Color::new(1.6, 0.7, 1.0), c1 + c2);
     }
+
+    #[test]
+    fn subtracting_colors() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+
+        assert_eq!(Color::new(0.2, 0.5, 0.5), c1 - c2);
+    }
 }
