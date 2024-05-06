d7d6009c6d9cb72de9449e3dcce10a4f85a95059
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-06 16:13:50 -0300

    [ � ] | implements: operation - add > color (app::canvas::math)

--- the_ray_tracer_challenge/src/canvas/math/mod.rs
+++ the_ray_tracer_challenge/src/canvas/math/mod.rs
@@ -1,3 +1,15 @@
 use std::ops::Add;
 
 use super::model::color::Color;
+
+impl Add for Color {
+    type Output = Color;
+
+    fn add(self, other: Color) -> Self::Output {
+        Color::new(
+            self.red + other.red,
+            self.green + other.green,
+            self.blue + other.blue,
+        )
+    }
+}
