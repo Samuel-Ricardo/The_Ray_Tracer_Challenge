9e21f67c6c19a7eb8c16cc301b3d323a911c5c55
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-06 15:28:46 -0300

    [ :white_check_mark: ] | create: test - color > create (app::color)

---
+++ the_ray_tracer_challenge/src/canvas/test/color.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+
+    fn create_new_rgb_color() {
+        let c = Color::new(2.0, 0.5, 1.0);
+
+        assert_eq!(c.red, 2.0);
+        assert_eq!(c.green, 0.5);
+        assert_eq!(c.blue, 1.0);
+    }
+}
