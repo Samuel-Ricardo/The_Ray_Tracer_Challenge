fb9bde20e382326b77fdbd13b98b5edc6db12cbf
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-09 14:16:35 -0300

    [ :art: ] | create: model - canvas (canvas::model)

--- the_ray_tracer_challenge/src/canvas/model/mod.rs
+++ the_ray_tracer_challenge/src/canvas/model/mod.rs
@@ -1,1 +1,20 @@
-pub mod color;
+use self::color::Color;
+
+pub mod color;
+
+pub struct Canvas {
+    pub width: usize,
+    pub height: usize,
+
+    pixels: Vec<Color>,
+}
+
+impl Canvas {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            pixels: vec![Color::black(); width * height],
+        }
+    }
+}
