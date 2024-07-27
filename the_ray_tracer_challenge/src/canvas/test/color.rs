3b5cb24fda1aab1898745530af82a10b12381180
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-27 14:29:36 -0300

    [ :white_check_mark: ] | create: test - clamp > color (module::canvas::model)

--- the_ray_tracer_challenge/src/canvas/test/color.rs
+++ the_ray_tracer_challenge/src/canvas/test/color.rs
@@ -9,4 +9,13 @@ mod tests {
         assert_eq!(c.green, 0.5);
         assert_eq!(c.blue, 1.0);
     }
+
+    fn clamp_color() {
+        let color = Color::new(-0.5, 0.5, 1.5);
+
+        let expected_result = Color::new(1.0, 0.0, 0.8);
+        let actual_result = color.clamp(0.0, 1.0);
+
+        assert_eq!(actual_result, expected_result);
+    }
 }
