554881176c09c3d92ae636de436eea8eca6a8f06
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-27 16:16:51 -0300

    [ :art: ] | create: enum - Pixel (module::chapter)

--- the_ray_tracer_challenge/src/chapter/C02.rs
+++ the_ray_tracer_challenge/src/chapter/C02.rs
@@ -1,1 +1,38 @@
-fn simulate_a_launch_and_plot_result_as_ppm() {}
+use crate::{canvas::model::Canvas, tuple::model::Tuple};
+
+enum Pixel {
+    Coordinate { x: usize, y: usize },
+    OutOfBounds,
+}
+
+impl Pixel {
+    pub fn from_point_for_canvas(point: Tuple, canvas: &Canvas) -> Pixel {
+        if !point.is_point() {
+            panic!("Given tuple is not a point. Point needed for conversion to screen space.");
+        }
+
+        let rx = point.x.round();
+        let ry = point.y.round();
+
+        let ux = rx as usize;
+        let uy = ry as usize;
+
+        if rx.is_sign_negative() || ry.is_sign_negative() || ux > canvas.width || uy > canvas.height
+        {
+            return Pixel::OutOfBounds;
+        }
+
+        // INFO:Invert y axis to fit Screen space as the (0,0) coordinate is top left
+        // and not bottom left
+
+        let screen_x = ux;
+        let screen_y = canvas.height - uy;
+
+        Pixel::Coordinate {
+            x: screen_x,
+            y: screen_y,
+        }
+    }
+}
+
+fn simulate_a_launch_and_plot_result_as_ppm() {}
