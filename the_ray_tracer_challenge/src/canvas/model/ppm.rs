429ed75cd2a17ca9586a7d70ae18fee4c220f0c3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-27 14:17:23 -0300

    [ :floppy_disk: ] | create: trait - PpmConvertible > build [header] (module::canvas)

--- the_ray_tracer_challenge/src/canvas/model/ppm.rs
+++ the_ray_tracer_challenge/src/canvas/model/ppm.rs
@@ -1,3 +1,21 @@
-trait PPM_Convertible {
-    fn to_ppm(&self) -> String;
+use super::Sized;
+
+const FILE_TYPE_IDENTIFIER: &str = "P3\n";
+const MAX_COLOR_VALUE: &str = "255\n";
+
+pub trait PpmConvertible {
+    fn build_ppm_header(&self) -> Vec<u8>
+    where
+        Self: Sized,
+    {
+        let mut header = Vec::new();
+
+        header.extend(FILE_TYPE_IDENTIFIER.as_bytes());
+        header.extend(format!("{} {}\n", self.width(), self.height()).into_bytes());
+        header.extend(MAX_COLOR_VALUE.as_bytes());
+
+        header
+    }
+
+    fn to_ppm(&self) -> Vec<u8>;
 }
