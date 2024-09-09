04d99557149d55f4af07f6a803b5216d576d7717
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-09 09:43:22 -0300

    [ :safety_vest: ] | create: module - equality (module::util)

--- the_ray_tracer_challenge/src/utils/equality.rs
+++ the_ray_tracer_challenge/src/utils/equality.rs
@@ -4,3 +4,8 @@ pub trait FuzzyEq<T: Clone> {
         !self.fuzzy_eq(right)
     }
 }
+
+pub fn f64_fuzzy_eq(left: f64, right: f64) -> bool {
+    const EPSILON: f64 = 0.00001;
+    (left - right).abs() < EPSILON
+}
