1728f2bdb6ab60084c2875073378f48a2472e426
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-09-09 10:24:36 -0300

    [ :recycle: ] | refact: f64 fuzzy eq

--- the_ray_tracer_challenge/src/utils/equality.rs
+++ the_ray_tracer_challenge/src/utils/equality.rs
@@ -1,9 +1,9 @@
 use super::EPSILON;
 
 pub trait FuzzyEq<T: Clone> {
-    fn fuzzy_eq(&self, right: T) -> bool;
-    fn fuzzy_ne(&self, right: T) -> bool {
-        !self.fuzzy_eq(right)
+    fn fuzzy_eq(&self, other: T) -> bool;
+    fn fuzzy_ne(&self, other: T) -> bool {
+        !self.fuzzy_eq(other)
     }
 }
 
