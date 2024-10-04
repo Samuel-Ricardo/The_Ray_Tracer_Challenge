f3892c006c5e1f06cc4d27afe5073d8d1de7f4af
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-04 17:44:28 -0300

    [ :abacus: ] | create: method - matrix > submatrix [3x3] (module::matrix::math)

--- the_ray_tracer_challenge/src/matrix/math/mod.rs
+++ the_ray_tracer_challenge/src/matrix/math/mod.rs
@@ -19,3 +19,41 @@ impl Matrix<2> {
         (self[0][0] * self[1][1]) - (self[0][1] * self[1][0])
     }
 }
+
+impl Matrix<3> {
+    pub fn submatrix(&self, row: usize, column: usize) -> Matrix<2> {
+        let mut matrix: Matrix<2> = Matrix::new();
+
+        let mut source_row = 0;
+        let mut source_column = 0;
+
+        let mut target_row = 0;
+        let mut target_column = 0;
+
+        while target_row < 2 {
+            if source_row == row {
+                source_row += 1;
+            }
+
+            while target_column < 2 {
+                if source_row == row || source_column == column {
+                    source_column += 1;
+                    continue;
+                }
+
+                matrix[target_row][target_column] = self[source_row][source_column];
+
+                source_column += 1;
+                target_column += 1;
+            }
+
+            source_row += 1;
+            target_row += 1;
+
+            target_column = 0;
+            source_column = 0;
+        }
+
+        return matrix;
+    }
+}
