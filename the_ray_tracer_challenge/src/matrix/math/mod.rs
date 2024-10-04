f4fa9af2223ebb1c16d410b21462c238ad0f9aa6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-04 17:23:42 -0300

    [ :abacus: ] | create: method - matrix<2> > determinat [math] (module::matrix::math)

--- the_ray_tracer_challenge/src/matrix/math/mod.rs
+++ the_ray_tracer_challenge/src/matrix/math/mod.rs
@@ -13,3 +13,9 @@ impl<const D: usize> Matrix<D> {
         matrix
     }
 }
+
+impl Matrix<2> {
+    pub fn determinant(&self) -> f64 {
+        (self[0][0] * self[1][1]) - (self[0][1] * self[1][0])
+    }
+}
