7981218f1bbe06d6476d35c305bf809a0aadbfe7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-04 17:20:34 -0300

    [ :white_check_mark: ] | test: matrix - determinant (module::matrix::math)

--- the_ray_tracer_challenge/src/matrix/test/math/mod.rs
+++ the_ray_tracer_challenge/src/matrix/test/math/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
-    use crate::matrix::model::Matrix;
+    use crate::{
+        assert_fuzzy_eq, assert_fuzzy_ne, matrix::model::Matrix, utils::equality::FuzzyEq,
+    };
 
     #[test]
     fn transpose_a_4x4_matrix() {
@@ -19,6 +21,16 @@ mod tests {
         ]);
 
         let actual_result = m.transpose();
-        assert_eq!(actual_result, expected_result);
+        assert_eq!(actual_result.fuzzy_eq(expected_result), true);
+    }
+
+    #[test]
+    fn determinant_of_a_2x2_matrix() {
+        let m = Matrix::from([[1.0, 5.0], [-3.0, 2.0]]);
+
+        let expected_result = 17.0;
+        let actual_result = m.determinant();
+
+        assert_fuzzy_eq!(actual_result, expected_result);
     }
 }
