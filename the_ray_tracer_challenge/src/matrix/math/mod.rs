5a452225a8ad18b7eba06d30c7d86f4036f40938
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-10-18 07:36:28 -0300

    [ :abacus: ] | impl: mul - matrix (module::matrix::math)

--- the_ray_tracer_challenge/src/matrix/math/mod.rs
+++ the_ray_tracer_challenge/src/matrix/math/mod.rs
@@ -1,3 +1,5 @@
+use crate::tuple::model::Tuple;
+
 use super::model::Matrix;
 
 pub mod x2;
@@ -15,5 +17,23 @@ impl<const D: usize> Matrix<D> {
         }
 
         matrix
+    }
+}
+
+impl<const D: usize> Mul<Matrix<D>> for Matrix<D> {
+    type Output = Matrix<D>;
+
+    fn mul(self, other: Matrix<D>) -> Self::Output {
+        let mut matrix = Matrix::new();
+
+        for row in 0..D {
+            for column in 0..D {
+                for i in 0..D {
+                    matrix[row][column] += self[row][i] * other[i][column];
+                }
+            }
+        }
+
+        return matrix;
     }
 }
