cb4b90bc16df16ae61b190f5146bec7865e41927
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-25 17:54:55 -0300

    [ :white_check_mark: ] | test: matrix - rotation [x] > point  (math::matrix)

--- the_ray_tracer_challenge/src/matrix/test/math/translation.rs
+++ the_ray_tracer_challenge/src/matrix/test/math/translation.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod matrix_translation_test {
+    use std::f64::consts::PI;
+
     use crate::{matrix::model::Matrix, tuple::model::Tuple};
 
     #[test]
@@ -76,6 +78,28 @@ mod matrix_translation_test {
 
         let expected_result = Tuple::point(-2.0, 3.0, 4.0);
         let actual_result = transform * p;
+
+        assert_eq!(actual_result, expected_result);
+    }
+
+    #[test]
+    fn rotating_a_point_around_the_x_axis_half_quarter() {
+        let half_quarter = Matrix::rotation_x(PI / 4.0);
+        let p = Tuple::point(0.0, 1.0, 0.0);
+
+        let expected_result = Tuple::point(0.0, 2.0_f64.sqrt() / 2.0, 2.0_f64.sqrt() / 2.0);
+        let actual_result = half_quarter * p;
+
+        assert_eq!(actual_result, expected_result);
+    }
+
+    #[test]
+    fn rotating_a_point_around_the_x_axis_full_quarter() {
+        let full_quarter = Matrix::rotation_x(PI / 2.0);
+        let p = Tuple::point(0.0, 1.0, 0.0);
+
+        let expected_result = Tuple::point(0.0, 0.0, 1.0);
+        let actual_result = full_quarter * p;
 
         assert_eq!(actual_result, expected_result);
     }
