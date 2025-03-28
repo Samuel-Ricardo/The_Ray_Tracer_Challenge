280e7abda5511134fc7866d963c53d597c93d4e6
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-28 08:16:32 -0300

    [ :white_check_mark: ] | test: matrix - rotation > inverse (math::matrix)

--- the_ray_tracer_challenge/src/matrix/test/math/rotation.rs
+++ the_ray_tracer_challenge/src/matrix/test/math/rotation.rs
@@ -39,4 +39,15 @@ mod matrix_scaling_test {
 
         assert_eq!(&actual_result, &expected_result);
     }
+
+    #[test]
+    fn the_inverse_of_an_x_rotation_rotates_in_the_opposite_direction_full_quarter() {
+        let invers_full_quarter = Matrix::rotation_x(PI / 2.0).inverse();
+        let point = Tuple::point(0.0, 1.0, 0.0);
+
+        let expected_result = Tuple::point(0.0, 0.0, -1.0);
+        let actual_result = invers_full_quarter * point;
+
+        assert_eq!(&actual_result, &expected_result);
+    }
 }
