f5e19c1cb9d772faaa88e15e4c7cfd4e1cca2f67
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-05-01 17:20:17 -0300

    [ :white_check_mark: ] | test: matrix - transformations > in sequence (math::matrix)

--- the_ray_tracer_challenge/src/matrix/test/math/transformation.rs
+++ the_ray_tracer_challenge/src/matrix/test/math/transformation.rs
@@ -2,6 +2,8 @@
 mod matrix_shearing_test {
     use std::f64::consts::PI;
 
+    use png::Transformations;
+
     use crate::{
         assert_fuzzy_eq, matrix::model::Matrix, tuple::model::Tuple, utils::equality::FuzzyEq,
     };
@@ -21,5 +23,20 @@ mod matrix_shearing_test {
 
         let translated_point = translation * scaled_point;
         assert_eq!(translated_point, Tuple::point(15.0, 0.0, 7.0));
+    }
+
+    #[test]
+    fn chained_transformations_must_be_applied_in_reverse_order() {
+        let point = Tuple::point(1.0, 0.0, 1.0);
+        let rotation_x = Matrix::rotation_x(PI / 2.0);
+        let scaling = Matrix::scaling(5.0, 5.0, 5.0);
+        let translation = Matrix::translation(10.0, 5.0, 7.0);
+
+        let transform = translation * scaling * rotation_x;
+
+        let actual_result = transform * point;
+        let expected_result = Tuple::point(15.0, 0.0, 7.0);
+
+        assert_fuzzy_eq!(actual_result, expected_result);
     }
 }
