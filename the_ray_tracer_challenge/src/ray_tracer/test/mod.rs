a54e3485e98a6f3cdc299e2b4c4af0a1ce79bdb7
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-05-03 14:35:34 -0300

    [ :white_check_mark: ] | test: sphere - ray > intersection at 2 points (module::raytracer)

--- the_ray_tracer_challenge/src/ray_tracer/test/mod.rs
+++ the_ray_tracer_challenge/src/ray_tracer/test/mod.rs
@@ -1,1 +1,2 @@
-mod ray;
+mod ray;
+mod sphere;
---
+++ the_ray_tracer_challenge/src/ray_tracer/test/sphere.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod sphere_test {
+    use crate::{
+        ray_tracer::{ray::Ray, sphere::Sphere},
+        tuple::model::Tuple,
+    };
+
+    #[test]
+    fn a_ray_intersects_a_sphere_at_two_points() {
+        let ray = Ray::new(Tuple::point(0.0, 0.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let sphere = Sphere::new(None);
+
+        let intersections = sphere.intersect(ray);
+
+        assert_eq!(2, intersections.len());
+        assert_eq!(4.0, intersections[0].value);
+        assert_eq!(6.0, intersections[1].value);
+    }
+}
