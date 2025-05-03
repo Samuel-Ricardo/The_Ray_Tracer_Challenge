82a291890c77742928933b544f57093e2769f0ca
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-05-03 14:31:36 -0300

    [ :abacus: ] | calculate: sphere - intersection (module::ray_tracer)

--- the_ray_tracer_challenge/src/ray_tracer/sphere.rs
+++ the_ray_tracer_challenge/src/ray_tracer/sphere.rs
@@ -1,4 +1,6 @@
-use crate::matrix::model::Matrix;
+use crate::{matrix::model::Matrix, tuple::model::Tuple};
+
+use super::{intersection::Intersection, ray::Ray};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Sphere {
@@ -13,5 +15,27 @@ impl Sphere {
             },
             Some(transform) => Sphere { transform },
         }
+    }
+
+    pub fn intersect(&self, ray: Ray) -> Vec<Intersection> {
+        let object_space_ray = ray.transform(self.transform.inverse());
+
+        let sphere_to_ray = object_space_ray.origin - Tuple::point(0.0, 0.0, 0.0);
+        let a = object_space_ray.direction.dot(&object_space_ray.direction);
+        let b = 2.0 * object_space_ray.direction.dot(&sphere_to_ray);
+        let c = sphere_to_ray.dot(&sphere_to_ray) - 1.0;
+        let descriminant = b.powi(2) - 4.0 * a * c;
+
+        if descriminant < 0.0 {
+            return vec![];
+        }
+
+        let intersection1 = (-b - descriminant.sqrt()) / (2.0 * a);
+        let intersection2 = (-b + descriminant.sqrt()) / (2.0 * a);
+
+        vec![
+            Intersection::new(intersection1),
+            Intersection::new(intersection2),
+        ]
     }
 }
