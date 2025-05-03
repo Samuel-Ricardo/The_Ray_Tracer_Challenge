f45f85d518c9e2f8d4fbb6b911189dfb7cacca3c
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-05-03 14:38:00 -0300

    [ :white_check_mark: ] | test: sphere - ray > origin from sphere (module::raytracer)

--- the_ray_tracer_challenge/src/ray_tracer/test/sphere.rs
+++ the_ray_tracer_challenge/src/ray_tracer/test/sphere.rs
@@ -38,4 +38,16 @@ mod sphere_test {
 
         assert_eq!(0, intersections.len());
     }
+
+    #[test]
+    fn a_ray_originates_inside_a_sphere() {
+        let ray = Ray::new(Tuple::point(0.0, 0.0, 0.0), Tuple::vector(0.0, 0.0, 1.0));
+        let sphere = Sphere::new(None);
+
+        let intersections = sphere.intersect(ray);
+
+        assert_eq!(2, intersections.len());
+        assert_eq!(-1.0, intersections[0].value);
+        assert_eq!(1.0, intersections[1].value);
+    }
 }
