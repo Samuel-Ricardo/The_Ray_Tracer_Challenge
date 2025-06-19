09b34810f028fb1e113912cc6433433c36c51bec
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-19 15:49:55 -0300

    [ :triangular_ruler: ] | create: shape - intersect > computed (module::shape)

---
+++ the_ray_tracer_challenge/src/shape/intersect/computed.rs
@@ -0,0 +1,34 @@
+use super::intersection::Intersection;
+use crate::tuple::model::Tuple;
+
+pub struct ComputedIntersection<'i> {
+    pub intersection: &'i Intersection,
+    pub point: Tuple,
+    pub over_point: Tuple,
+    pub normal_v: Tuple,
+    pub eye_v: Tuple,
+    pub reflect_v: Tuple,
+    pub inside: bool,
+}
+
+impl<'i> ComputedIntersection<'i> {
+    pub fn new(
+        intersection: &'i Intersection,
+        point: Tuple,
+        over_point: Tuple,
+        normal_v: Tuple,
+        eye_v: Tuple,
+        reflect_v: Tuple,
+        inside: bool,
+    ) -> Self {
+        ComputedIntersection {
+            intersection,
+            point,
+            over_point,
+            normal_v,
+            eye_v,
+            reflect_v,
+            inside,
+        }
+    }
+}
