84859e033a7c5aaff2939010d8f20d75e37f6921
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-25 16:33:55 -0300

    [ :triangular_ruler: ] | impl: sphere - intersectable > to material (module::body)

--- the_ray_tracer_challenge/src/shape/body/sphere.rs
+++ the_ray_tracer_challenge/src/shape/body/sphere.rs
@@ -73,4 +73,8 @@ impl Intersectable for Sphere {
     fn normal_at_in_object_space(&self, object_space_point: Tuple) -> Tuple {
         (object_space_point - Tuple::zero_point()).normalize()
     }
+
+    fn material(&self) -> Material {
+        self.material
+    }
 }
