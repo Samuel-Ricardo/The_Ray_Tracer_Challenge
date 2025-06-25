df0846004685b79b2dcd15440657f68740dee0d5
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-25 16:24:50 -0300

    [ :triangular_ruler: ] | impl: sphere - intersectable > intersect in object space (module::body)

--- the_ray_tracer_challenge/src/shape/body/sphere.rs
+++ the_ray_tracer_challenge/src/shape/body/sphere.rs
@@ -1,4 +1,12 @@
-use crate::{matrix::model::Matrix, shape::material::Material, utils::equality::FuzzyEq};
+use crate::{
+    matrix::model::Matrix,
+    ray_tracer::ray::Ray,
+    shape::{intersect::intersectables::Intersectable, material::Material},
+    tuple::model::Tuple,
+    utils::equality::FuzzyEq,
+};
+
+use super::Body;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Sphere {
@@ -37,5 +45,28 @@ impl Sphere {
 impl FuzzyEq<&Sphere> for Sphere {
     fn fuzzy_eq(&self, other: &Sphere) -> bool {
         self.transform.fuzzy_eq(other.transform) && self.material.fuzzy_eq(other.material)
+    }
+}
+
+impl Intersectable for Sphere {
+    fn intersect_in_object_space(&self, object_space_ray: Ray) -> Vec<(crate::f64, Body)> {
+        let sphere_to_ray = object_space_ray.origin - Tuple::zero_point();
+
+        let a = object_space_ray.direction.dot(&object_space_ray.direction);
+        let b = 2.0 * object_space_ray.direction.dot(&sphere_to_ray);
+        let c = sphere_to_ray.dot(&sphere_to_ray) - 1.0;
+        let descriminant = b.powi(2) - 4.0 * a * c;
+
+        if descriminant < 0.0 {
+            vec![]
+        } else {
+            let first_intersection = (-b - descriminant.sqrt()) / (2.0 * a);
+            let second_intersection = (-b + descriminant.sqrt()) / (2.0 * a);
+
+            vec![
+                (first_intersection, Body::from(*self)),
+                (second_intersection, Body::from(*self)),
+            ]
+        }
     }
 }
