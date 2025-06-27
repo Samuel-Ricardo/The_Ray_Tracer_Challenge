fe032b62d7d75eef0eb9e979a3e74f81740c69a8
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-26 23:42:45 -0300

    [ :sparkles: ] | impl: material - phong > FuzzyEq (module::shape)

--- the_ray_tracer_challenge/src/shape/material/phong.rs
+++ the_ray_tracer_challenge/src/shape/material/phong.rs
@@ -26,3 +26,16 @@ impl Default for Phong {
         }
     }
 }
+
+impl FuzzyEq<Phong> for Phong {
+    fn fuzzy_eq(&self, other: Phong) -> bool {
+        self.color.fuzzy_eq(other.color)
+            && self.ambient.fuzzy_eq(other.ambient)
+            && self.diffuse.fuzzy_eq(other.diffuse)
+            && self.specular.fuzzy_eq(other.specular)
+            && self.shininess.fuzzy_eq(other.shininess)
+            && self
+                .pattern
+                .is_some_and(|p| p.fuzzy_eq(other.pattern.unwrap()))
+    }
+}
