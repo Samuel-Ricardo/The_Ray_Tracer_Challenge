a13d070583103a3ffa2195d09a4a588cb02e0e50
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-26 23:42:28 -0300

    [ :sparkles: ] | impl: material - phong > default (module::shape)

--- the_ray_tracer_challenge/src/shape/material/phong.rs
+++ the_ray_tracer_challenge/src/shape/material/phong.rs
@@ -1,4 +1,4 @@
-use crate::canvas::model::color::Color;
+use crate::{canvas::model::color::Color, utils::equality::FuzzyEq};
 
 use super::pattern::Patter;
 
@@ -9,6 +9,20 @@ pub struct Phong {
     pub ambient: f64,
     pub diffuse: f64,
     pub specular: f64,
-    pub shiness: f64,
+    pub shininess: f64,
     pub reflectiveness: f64,
+}
+
+impl Default for Phong {
+    fn default() -> Self {
+        Self {
+            color: Color::new(1.0, 1.0, 1.0),
+            pattern: None,
+            ambient: 0.1,
+            diffuse: 0.9,
+            specular: 0.9,
+            shininess: 200.0,
+            reflectiveness: 0.0,
+        }
+    }
 }
