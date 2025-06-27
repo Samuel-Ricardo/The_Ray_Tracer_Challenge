b06059570db97d3a4d7af6422f4d6a40796cfc2e
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-26 23:36:04 -0300

    [ :syringe: ] | create: module - shape (module::main)

--- the_ray_tracer_challenge/src/main.rs
+++ the_ray_tracer_challenge/src/main.rs
@@ -1,13 +1,15 @@
 use chapter::{C02::simulate_a_launch_and_plot_result_as_ppm, C03::rendering_a_clock_cycle};
 
+#[macro_use]
+
 mod canvas;
 mod chapter;
 mod matrix;
-mod scene;
-mod tuple;
-#[macro_use]
-mod utils;
 mod ray_tracer;
+mod scene;
+mod shape;
+mod tuple;
+mod utils;
 
 fn main() {
     // NOTE: [DEMO] | CHAPTER - 01 => simulate_physics_with_a_launch();
