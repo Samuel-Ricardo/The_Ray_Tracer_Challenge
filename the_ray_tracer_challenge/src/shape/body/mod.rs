ec7162ed0ac10c58bbf66c26454f70a69f8b08aa
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-28 10:42:19 -0300

    [ :sparkles: ] | update: body - instance (module::shape)

--- the_ray_tracer_challenge/src/shape/body/mod.rs
+++ the_ray_tracer_challenge/src/shape/body/mod.rs
@@ -1,5 +1,46 @@
+pub mod plane;
+pub mod sphere;
+
+use self::sphere::Sphere;
+
+use super::intersect::intersectables::Intersectable;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Body {
-    Sphere(),
+    Sphere(Sphere),
     Plane(),
+}
+
+impl From<Sphere> for Body {
+    fn from(sphere: Sphere) -> Self {
+        Body::Sphere(sphere)
+    }
+}
+
+impl Intersectable for Body {
+    fn normal_at(&self, point: crate::tuple::model::Tuple) -> crate::tuple::model::Tuple {
+        todo!()
+    }
+
+    fn material(&self) -> super::material::Material {
+        todo!()
+    }
+
+    fn transform(&self) -> crate::matrix::model::Matrix<4> {
+        todo!()
+    }
+
+    fn intersect_in_object_space(
+        &self,
+        object_space_ray: crate::ray_tracer::ray::Ray,
+    ) -> Vec<(f64, Self)> {
+        todo!()
+    }
+
+    fn normal_at_in_object_space(
+        &self,
+        object_space_point: crate::tuple::model::Tuple,
+    ) -> crate::tuple::model::Tuple {
+        todo!()
+    }
 }
